9abc1a1f42130ff6feb429002e2b0805074adc8b
Paul Merrill <pdm@pdm.me>
2014-12-24 13:05:07 -0800

    grove01: add wandering NPC wizard
    Introduce AIWander lambas for tick and turn movement modes.

---
+++ src/ai/ai-wander.cpp
@@ -0,0 +1,79 @@
+/**********************************
+** Tsunagari Tile Engine         **
+** ai-wander.cpp                 **
+** Copyright 2014 PariahSoft LLC **
+**********************************/
+
+// **********
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+// **********
+
+#include <assert.h>
+
+#include "../../TsunagariC/src/character.h"
+#include "../../TsunagariC/src/client-conf.h"
+#include "../../TsunagariC/src/cooldown.h"
+#include "../../TsunagariC/src/random.h"
+
+#include "ai-wander.h"
+
+//! Move the character.
+static void doMove(Character& c)
+{
+	ivec2 facing = { 0, 0 };
+	switch (randInt(0, 3)) {
+	case 0: facing = { -1, 0 }; break;
+	case 1: facing = { +1, 0 }; break;
+	case 2: facing = { 0, -1 }; break;
+	case 3: facing = { 0, +1 }; break;
+	}
+	c.moveByTile(facing);
+}
+
+//! Decide whether or not to move.
+static void maybeMove(Character& c, int chance)
+{
+	if (randInt(1, chance) == 1)
+		doMove(c);
+}
+
+std::function<void (time_t)>
+AIWanderTile(Character& c, int chance, time_t tryEvery)
+{
+	assert(conf.moveMode == TILE);
+
+	Cooldown cooldown(tryEvery);
+	return [&c, chance, cooldown] (time_t dt) mutable {
+		cooldown.advance(dt);
+		if (cooldown.hasExpired()) {
+			cooldown.wrap();
+			maybeMove(c, chance);
+		}
+	};
+}
+
+std::function<void ()>
+AIWanderTurn(Character& c, int chance)
+{
+	assert(conf.moveMode == TURN);
+
+	return [&c, chance] () mutable {
+		maybeMove(c, chance);
+	};
+}
--- src/areas/grove01.cpp
+++ src/areas/grove01.cpp
@@ -1,8 +1,13 @@
 #include "../../TsunagariC/src/area.h"
+#include "../../TsunagariC/src/log.h"
+#include "../../TsunagariC/src/npc.h"
+#include "../../TsunagariC/src/overlay.h"
 #include "../../TsunagariC/src/random.h"
 #include "../../TsunagariC/src/timer.h"
 
 #include "../../TsunagariC/src/data/area.h"
+
+#include "../ai/ai-wander.h"
 
 class grove01 : public DataArea
 {
@@ -15,37 +20,42 @@ public:
 	}
 
 	void onLoad() {
-		auto cloud = area->spawnOverlay("entities/cloud/cloud.xml", 11, 2, 10.0, "down");
-		cloud->move(-400, 0);
+		// Create a wandering wizard NPC.
+		NPC* wizard = area->spawnNPC("entities/wizard/wizard.xml",
+			vicoord(6, 3, 0.0), "down");
+		auto wanderBehavior = AIWanderTile(*wizard, 4, 1000);
+		wizard->attach(std::move(wanderBehavior));
+
+		// And a drifting cloud Overlay.
+		Overlay* cloud = area->spawnOverlay("entities/cloud/cloud.xml",
+			vicoord(11, 2, 10.0), "down");
+		cloud->drift(ivec2(-400, 0));
 	}
 
 	void onWell(Entity&, Tile&) {
 		static constexpr int maxAlpha = 192;
 
-		if (drinking == false) {
-			drinking = true;
+		if (drinking)
+			return;
 
-			wellTimer.reset();
-			wellTimer.setRunning(true);
+		drinking = true;
 
-			auto& sound = SoundManager::instance();
-			auto splash = sound.play("sounds/splash.oga");
-			splash->setSpeed(1.0 + randFloat(-0.1, 0.1));
+		SoundManager::instance().play("sounds/splash.oga")
+			->setSpeed(1.0 + randFloat(-0.1, 0.1));
 
-			timerProgressAndThen(1000,
-				[&] (double percent) {
-					uint8_t alpha;
-					if (percent < 0.5)
-						alpha = (uint8_t)(2 * maxAlpha * percent);
-					else
-						alpha = (uint8_t)(2 * maxAlpha * (1 - percent));
-					area->setColorOverlay(alpha, 255, 255, 255);
-				},
-				[&] () {
-					area->setColorOverlay(0, 0, 0, 0);
-					drinking = false;
-				}
-			);
-		}
+		timerProgressAndThen(1000,
+			[&] (double percent) {
+				uint8_t alpha;
+				if (percent < 0.5)
+					alpha = (uint8_t)(2 * maxAlpha * percent);
+				else
+					alpha = (uint8_t)(2 * maxAlpha * (1 - percent));
+				area->setColorOverlay(alpha, 255, 255, 255);
+			},
+			[&] () {
+				area->setColorOverlay(0, 0, 0, 0);
+				drinking = false;
+			}
+		);
 	}
 };
